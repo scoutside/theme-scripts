ca65fde96ceef3c3ac51c6dc200a6c8ab9a069af
Adrian Nuyda <adamnuyda@yahoo.com>
2022-05-17 10:38:59 -0700

    supports selling plan options

--- packages/theme-cart/theme-cart.js
+++ packages/theme-cart/theme-cart.js
@@ -6,196 +6,202 @@
  * @namespace cart
  */
 
-import * as request from './request';
-import * as validate from './validate';
+ import * as request from './request';
+ import * as validate from './validate';
 
-/**
- * Returns the state object of the cart
- * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
- */
-export function getState() {
-  return request.cart();
-}
+ /**
+  * Returns the state object of the cart
+  * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
+  */
+ export function getState() {
+   return request.cart();
+ }
 
-/**
- * Returns the index of the cart line item
- * @param {string} key The unique key of the line item
- * @returns {Promise} Resolves with the index number of the line item
- */
-export function getItemIndex(key) {
-  validate.key(key);
+ /**
+  * Returns the index of the cart line item
+  * @param {string} key The unique key of the line item
+  * @returns {Promise} Resolves with the index number of the line item
+  */
+ export function getItemIndex(key) {
+   validate.key(key);
 
-  return request.cart().then(function(state) {
-    var index = -1;
+   return request.cart().then(function(state) {
+     var index = -1;
 
-    state.items.forEach(function(item, i) {
-      index = item.key === key ? i + 1 : index;
-    });
+     state.items.forEach(function(item, i) {
+       index = item.key === key ? i + 1 : index;
+     });
 
-    if (index === -1) {
-      return Promise.reject(
-        new Error('Theme Cart: Unable to match line item with provided key')
-      );
-    }
+     if (index === -1) {
+       return Promise.reject(
+         new Error('Theme Cart: Unable to match line item with provided key')
+       );
+     }
 
-    return index;
-  });
-}
+     return index;
+   });
+ }
 
-/**
- * Fetches the line item object
- * @param {string} key The unique key of the line item
- * @returns {Promise} Resolves with the line item object (See response of cart/add.js https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#add-to-cart)
- */
-export function getItem(key) {
-  validate.key(key);
+ /**
+  * Fetches the line item object
+  * @param {string} key The unique key of the line item
+  * @returns {Promise} Resolves with the line item object (See response of cart/add.js https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#add-to-cart)
+  */
+ export function getItem(key) {
+   validate.key(key);
 
-  return request.cart().then(function(state) {
-    var lineItem = null;
+   return request.cart().then(function(state) {
+     var lineItem = null;
 
-    state.items.forEach(function(item) {
-      lineItem = item.key === key ? item : lineItem;
-    });
+     state.items.forEach(function(item) {
+       lineItem = item.key === key ? item : lineItem;
+     });
 
-    if (lineItem === null) {
-      return Promise.reject(
-        new Error('Theme Cart: Unable to match line item with provided key')
-      );
-    }
+     if (lineItem === null) {
+       return Promise.reject(
+         new Error('Theme Cart: Unable to match line item with provided key')
+       );
+     }
 
-    return lineItem;
-  });
-}
+     return lineItem;
+   });
+ }
 
-/**
- * Add a new line item to the cart
- * @param {number} id The variant's unique ID
- * @param {object} options Optional values to pass to /cart/add.js
- * @param {number} options.quantity The quantity of items to be added to the cart
- * @param {object} options.properties Line item property key/values (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-properties)
- * @returns {Promise} Resolves with the line item object (See response of cart/add.js https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#add-to-cart)
- */
-export function addItem(id, options) {
-  options = options || {};
+ /**
+  * Add a new line item to the cart
+  * @param {number} id The variant's unique ID
+  * @param {object} options Optional values to pass to /cart/add.js
+  * @param {number} options.quantity The quantity of items to be added to the cart
+  * @param {object} options.properties Line item property key/values (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-properties)
+  * @returns {Promise} Resolves with the line item object (See response of cart/add.js https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#add-to-cart)
+  */
+ export function addItem(id, options) {
+   options = options || {};
 
-  validate.id(id);
+   validate.id(id);
 
-  return request.cartAdd(id, options.quantity, options.properties);
-}
+   return request.cartAdd(id, options.quantity, options.properties, options.selling_plan);
+ }
 
-/**
- * Add a new line item to the cart from a product form
- * @param {object} form DOM element which is equal to the <form> node
- * @returns {Promise} Resolves with the line item object (See response of cart/add.js https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#add-to-cart)
- */
-export function addItemFromForm(form) {
-  validate.form(form);
+ export function addItems(items) {
+   items.forEach(({id}) => validate.id(id))
 
-  var formData = new FormData(form);
-  validate.id(parseInt(formData.get('id'), 10));
+   return request.cartAddBulk(items);
+ }
 
-  return request.cartAddFromForm(formData);
-}
+ /**
+  * Add a new line item to the cart from a product form
+  * @param {object} form DOM element which is equal to the <form> node
+  * @returns {Promise} Resolves with the line item object (See response of cart/add.js https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#add-to-cart)
+  */
+ export function addItemFromForm(form) {
+   validate.form(form);
 
-/**
- * Changes the quantity and/or properties of an existing line item.
- * @param {string} key The unique key of the line item (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-key)
- * @param {object} options Optional values to pass to /cart/add.js
- * @param {number} options.quantity The quantity of items to be added to the cart
- * @param {object} options.properties Line item property key/values (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-properties)
- * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
- */
-export function updateItem(key, options) {
-  validate.key(key);
-  validate.options(options);
+   var formData = new FormData(form);
+   validate.id(parseInt(formData.get('id'), 10));
 
-  return getItemIndex(key).then(function(line) {
-    return request.cartChange(line, options);
-  });
-}
+   return request.cartAddFromForm(formData);
+ }
 
-/**
- * Removes a line item from the cart
- * @param {string} key The unique key of the line item (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-key)
- * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
- */
-export function removeItem(key) {
-  validate.key(key);
+ /**
+  * Changes the quantity and/or properties of an existing line item.
+  * @param {string} key The unique key of the line item (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-key)
+  * @param {object} options Optional values to pass to /cart/add.js
+  * @param {number} options.quantity The quantity of items to be added to the cart
+  * @param {object} options.properties Line item property key/values (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-properties)
+  * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
+  */
+ export function updateItem(key, options) {
+   validate.key(key);
+   validate.options(options);
 
-  return getItemIndex(key).then(function(line) {
-    return request.cartChange(line, { quantity: 0 });
-  });
-}
+   return getItemIndex(key).then(function(line) {
+     return request.cartChange(line, options);
+   });
+ }
 
-/**
- * Sets all quantities of all line items in the cart to zero. This does not remove cart attributes nor the cart note.
- * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
- */
-export function clearItems() {
-  return request.cartClear();
-}
+ /**
+  * Removes a line item from the cart
+  * @param {string} key The unique key of the line item (https://help.shopify.com/en/themes/liquid/objects/line_item#line_item-key)
+  * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
+  */
+ export function removeItem(key) {
+   validate.key(key);
 
-/**
- * Gets all cart attributes
- * @returns {Promise} Resolves with the cart attributes object
- */
-export function getAttributes() {
-  return request.cart().then(function(state) {
-    return state.attributes;
-  });
-}
+   return getItemIndex(key).then(function(line) {
+     return request.cartChange(line, { quantity: 0 });
+   });
+ }
 
-/**
- * Sets all cart attributes
- * @returns {Promise} Resolves with the cart state object
- */
-export function updateAttributes(attributes) {
-  return request.cartUpdate({ attributes: attributes });
-}
+ /**
+  * Sets all quantities of all line items in the cart to zero. This does not remove cart attributes nor the cart note.
+  * @returns {Promise} Resolves with the state object of the cart (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-cart)
+  */
+ export function clearItems() {
+   return request.cartClear();
+ }
 
-/**
- * Clears all cart attributes
- * @returns {Promise} Resolves with the cart state object
- */
-export function clearAttributes() {
-  return getAttributes().then(function(attributes) {
-    for (var key in attributes) {
-      attributes[key] = '';
-    }
-    return updateAttributes(attributes);
-  });
-}
+ /**
+  * Gets all cart attributes
+  * @returns {Promise} Resolves with the cart attributes object
+  */
+ export function getAttributes() {
+   return request.cart().then(function(state) {
+     return state.attributes;
+   });
+ }
 
-/**
- * Gets cart note
- * @returns {Promise} Resolves with the cart note string
- */
-export function getNote() {
-  return request.cart().then(function(state) {
-    return state.note;
-  });
-}
+ /**
+  * Sets all cart attributes
+  * @returns {Promise} Resolves with the cart state object
+  */
+ export function updateAttributes(attributes) {
+   return request.cartUpdate({ attributes: attributes });
+ }
 
-/**
- * Sets cart note
- * @returns {Promise} Resolves with the cart state object
- */
-export function updateNote(note) {
-  return request.cartUpdate({ note: note });
-}
+ /**
+  * Clears all cart attributes
+  * @returns {Promise} Resolves with the cart state object
+  */
+ export function clearAttributes() {
+   return getAttributes().then(function(attributes) {
+     for (var key in attributes) {
+       attributes[key] = '';
+     }
+     return updateAttributes(attributes);
+   });
+ }
 
-/**
- * Clears cart note
- * @returns {Promise} Resolves with the cart state object
- */
-export function clearNote() {
-  return request.cartUpdate({ note: '' });
-}
+ /**
+  * Gets cart note
+  * @returns {Promise} Resolves with the cart note string
+  */
+ export function getNote() {
+   return request.cart().then(function(state) {
+     return state.note;
+   });
+ }
 
-/**
- * Get estimated shipping rates.
- * @returns {Promise} Resolves with response of /cart/shipping_rates.json (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-shipping-rates)
- */
-export function getShippingRates() {
-  return request.cartShippingRates();
-}
+ /**
+  * Sets cart note
+  * @returns {Promise} Resolves with the cart state object
+  */
+ export function updateNote(note) {
+   return request.cartUpdate({ note: note });
+ }
+
+ /**
+  * Clears cart note
+  * @returns {Promise} Resolves with the cart state object
+  */
+ export function clearNote() {
+   return request.cartUpdate({ note: '' });
+ }
+
+ /**
+  * Get estimated shipping rates.
+  * @returns {Promise} Resolves with response of /cart/shipping_rates.json (https://help.shopify.com/en/themes/development/getting-started/using-ajax-api#get-shipping-rates)
+  */
+ export function getShippingRates() {
+   return request.cartShippingRates();
+ }
